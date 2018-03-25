1a615f9f042ed24a790697089b1bd1013d622f76
Jehan <jehan.tremback@gmail.com>
2018-03-25 16:16:31 -0700

    add cors

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
+const fetch = require("node-fetch");
+const base = require("airtable").base("app10GFwKz0Ipn7TW");
 
 const airtableCache = {};
 
-function updateAirtableCache() {
-  airtableCache.currentTime = Date.now();
+async function updateAirtableCache() {
+  const nodes = [];
+  base("Requested Deployments")
+    .select()
+    .eachPage(
+      function page(records, fetchNextPage) {
+        records.forEach(function(record) {
+          const geocodeCache = record.get("Geocode Cache");
+          if (geocodeCache) {
+            const geocode = JSON.parse(
+              Buffer.from(geocodeCache.substring(2), "base64").toString("utf8")
+            );
+            nodes.push({ lat: geocode.o.lat, lng: geocode.o.lng });
+          }
+        });
+
+        fetchNextPage();
+      },
+      function done(err) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        airtableCache.nodes = nodes;
+      }
+    );
 }
 
 setInterval(updateAirtableCache, 1000);
 
 express()
-  .get("/", (req, res) => res.send(airtableCache.currentTime + ""))
+  .use(cors())
+  .get("/nodes", (req, res) => res.send(JSON.stringify(airtableCache.nodes)))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
