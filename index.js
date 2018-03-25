7db5bfcd53e754df2cb6d52ad5b4293bb379ada2
Jehan <jehan.tremback@gmail.com>
2018-03-24 19:19:45 -0700

    add fake airtable cache

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
 
+const airtableCache = {};
+
+function updateAirtableCache() {
+  airtableCache.currentTime = Date.now();
+}
+
+setInterval(updateAirtableCache, 1000);
+
 express()
-  .get("/", (req, res) => res.send("Hello"))
+  .get("/", (req, res) => res.send(airtableCache.currentTime))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
