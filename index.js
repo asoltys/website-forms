8079095395f72155c9a267f0fa21842446c7e313
Jehan <jehan.tremback@gmail.com>
2018-09-26 21:06:12 -0700

    start on posting stuff

--- index.js
+++ index.js
@@ -3,9 +3,21 @@ const cors = require("cors");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
 const fetch = require("node-fetch");
-const base = require("airtable").base("app10GFwKz0Ipn7TW");
+const base = require("airtable").base("appcgeWHNPAmxp88S");
+const bodyParser = require("body-parser");
 
 const airtableCache = {};
+
+const postToAirtable = (baseName, redirect) => (req, res) => {
+  console.log("req", req.body);
+  base(baseName).create({ ...req.body, IP: req.ip }, function(err) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  });
+  res.redirect(redirect);
+};
 
 async function updateAirtableCache() {
   const nodes = [];
@@ -36,9 +48,11 @@ async function updateAirtableCache() {
     );
 }
 
-setInterval(updateAirtableCache, 1000);
+// setInterval(updateAirtableCache, 1000);
 
 express()
   .use(cors())
+  .use(bodyParser.urlencoded({ extended: true }))
   .get("/nodes", (req, res) => res.send(JSON.stringify(airtableCache.nodes)))
+  .post("/organizer-submit-eyJpIj", postToAirtable("Organizers", "back"))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
